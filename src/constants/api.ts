27447928ac8a6a3d2b13064baecd78125acb1f31
emmaotuonye <emmanuel.otuonye@dlhosolutions.ng>
2024-06-24 13:33:40 +0100

    to be merged with main

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
 // export const baseURL =
 //   "https://carbon-adjust-api-7c3d3c0df018.herokuapp.com/api/v1";
 export const baseURL =
-  "https://carbon-adjust-dev-ff1d65869271.herokuapp.com/api/v1";
+  "https://carbon-adjust-api-v2-d77b19823594.herokuapp.com/api/v1";
