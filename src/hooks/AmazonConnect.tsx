984d44ce2c730a54c3a2e52b8da10703ed174153
emmaotuonye <emmanuel.otuonye@dlhosolutions.ng>
2024-06-25 17:53:03 +0100

    new commit

--- src/hooks/AmazonConnect.tsx
+++ src/hooks/AmazonConnect.tsx
@@ -6,32 +6,39 @@ const AmazonConnect = () => {
     const containerDiv = document.getElementById("container-div")!;
     const instanceURL = "https://carbon-adjust.my.connect.aws/ccp-v2/";
 
-    connect.core.initCCP(containerDiv, {
-      ccpUrl: instanceURL,
-      loginPopup: true,
-      loginPopupAutoClose: true,
-      loginOptions: {
-        autoClose: true,
-        height: 600,
-        width: 400,
-        top: 0,
-        left: 0,
-      },
-      region: "eu-west-2", // replace with the region where your Amazon Connect instance is hosted
-      softphone: {
-        allowFramedSoftphone: true,
-        disableRingtone: false,
-        ringtoneUrl: "./ringtone.mp3",
-      },
-      pageOptions: {
-        // Optional, allows customization of the CCP
-        enableAudioDeviceSettings: true, // Optional, default is true
-        enablePhoneTypeSettings: true, // Optional, default is true
-      },
-      ccpAckTimeout: 5000, // Optional, number of milliseconds to wait for acknowledgment
-      ccpSynTimeout: 3000, // Optional, number of milliseconds to wait for synchronization
-      ccpLoadTimeout: 10000,
-    });
+    if (!document.getElementById("ccpIframe")) {
+      try {
+        connect.core.initCCP(containerDiv, {
+          ccpUrl: instanceURL,
+          loginPopup: true,
+          loginPopupAutoClose: true,
+          loginOptions: {
+            autoClose: true,
+            height: 600,
+            width: 400,
+            top: 0,
+            left: 0,
+          },
+          region: "eu-west-2", // replace with the region where your Amazon Connect instance is hosted
+          softphone: {
+            allowFramedSoftphone: true,
+            disableRingtone: false,
+            allowFramedVideoCall: true,
+            // ringtoneUrl: "./ringtone.mp3",
+          },
+          pageOptions: {
+            // Optional, allows customization of the CCP
+            enableAudioDeviceSettings: true, // Optional, default is true
+            enablePhoneTypeSettings: true, // Optional, default is true
+          },
+          ccpAckTimeout: 5000, // Optional, number of milliseconds to wait for acknowledgment
+          ccpSynTimeout: 3000, // Optional, number of milliseconds to wait for synchronization
+          ccpLoadTimeout: 10000,
+        });
+      } catch (ex) {
+        console.error("Error initialising CCP", ex);
+      }
+    }
 
     connect.agent((agent) => {
       agent.onStateChange((state) => {
@@ -43,6 +50,7 @@ const AmazonConnect = () => {
   return (
     <div>
       <h1>Amazon Connect Integration</h1>
+
       <div id="container-div" style={{ width: "400px", height: "600px" }}></div>
     </div>
   );
