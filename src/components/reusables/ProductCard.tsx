b7614320b1af7a1257e1789b99bf60132b43d001
emmaotuonye <emmanuel.otuonye@dlhosolutions.ng>
2024-06-26 23:31:53 +0100

    new product card redesign

--- src/components/reusables/ProductCard.tsx
+++ src/components/reusables/ProductCard.tsx
@@ -3,6 +3,8 @@ import { IProduct } from "@/interfaces/product.interface";
 import { FaStar } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { GrFavorite } from "react-icons/gr";
+import { MdStarRate } from "react-icons/md";
 // import questions from "../../dummy/questions.json";
 
 interface Props extends IProduct {
@@ -20,15 +22,26 @@ const ProductCard = ({ isMerchant = false, ...props }: Props) => {
   };
 
   // console.log(props.category, "prod category");
+  console.log(props, "ndhh");
 
   return (
     <div className="min-w-[228px] group">
-      <div className="relative ">
-        {/* <span className="absolute top-[13px] left-[13px] w-[55px] h-[20px] grid place-items-center bg-[#BE0B0D] text-white text-[13.94px] font-[700] rounded-[3.31px]">
-          <span>Hot</span>
-        </span> */}
+      <div className="relative">
+        <div className="absolute top-[10px] right-[10px] rounded-full w-[32px] h-[32px] bg-white grid place-items-center z-[10]">
+          <GrFavorite />
+        </div>
 
-        <div className="relative">
+        <div className="absolute top-[13px] left-[13px] z-[10] flex flex-col gap-2">
+          <span className="w-[55px] h-[20px] grid place-items-center bg-white text-[13.94px] font-[700] rounded-[3.31px]">
+            <span>New</span>
+          </span>
+
+          <span className=" w-[55px] h-[20px] grid place-items-center bg-[#BE0B0D] text-white text-[13.94px] font-[700] rounded-[13.31px]">
+            <span>Hot</span>
+          </span>
+        </div>
+
+        <div className="relative h-[304px] border border-[#F3F5F7] grid place-items-center rounded-xl bg-[#F3F5F7]">
           <div className="hidden group-hover:flex flex-col absolute top-0 left-0 w-full h-full bg-[#000000] bg-opacity-20 rounded-lg">
             {!isMerchant && (
               <div className="mx-auto mt-auto h-fit pb-[16px] grid place-items-center w-full">
@@ -48,14 +61,20 @@ const ProductCard = ({ isMerchant = false, ...props }: Props) => {
           <img
             src={props?.attachments?.[0]}
             alt=""
-            className="w-[228px] h-[304px] rounded-lg object-cover"
+            // className="w-[228px] h-[304px] rounded-lg object-cover"
+            className="w-[100px] h-auto object-cover"
           />
         </div>
 
         <div className="gap-[3px] mt-1 flex flex-col">
-          <div className="flex-center gap-1">
+          <div className="flex-center gap-2">
+            <span className="text-xs font-[400]">
+              {/* @ts-ignore */}
+              {props.owner.name! || props.owner}
+            </span>
             {Array.from({ length: 5 }, (_, i) => (
-              <FaStar size={13.94} key={i} color="#0E89F7" />
+              // <FaStar size={13.94} key={i} color="#E99C1B" />
+              <MdStarRate size={13.94} key={i} color="#E99C1B" />
             ))}
           </div>
 
@@ -67,7 +86,9 @@ const ProductCard = ({ isMerchant = false, ...props }: Props) => {
             {props.title}
           </h2>
 
-          <h2 className="text-xs font-[600] ">{props.price}</h2>
+          <h2 className="text-xs font-[600] ">{` ${props.currency} ${
+            props.price ?? 0
+          }`}</h2>
         </div>
       </div>
     </div>
