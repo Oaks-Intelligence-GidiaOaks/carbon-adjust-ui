516039a365b3c170739d322b3a3779e01a009553
emmaotuonye <emmanuel.otuonye@dlhosolutions.ng>
2024-07-07 18:50:35 +0100

    hot fixes ui-mobile

--- src/components/reusables/Backdrop.tsx
+++ src/components/reusables/Backdrop.tsx
@@ -10,7 +10,7 @@ const Backdrop = (props: {
     <div
       className={`  ${
         props.show ? "fixed" : "hidden"
-      } top-0 left-0 w-full h-full bg-[#D9D9D9] bg-opacity-70  z-50 ${
+      } top-0 left-0 w-full h-full bg-[#D9D9D9] bg-opacity-70  z-[9999] ${
         props.classnames
       }`}
       // onClick={closeBackdrop}
--- src/components/reusables/ProductsCategory.tsx
+++ src/components/reusables/ProductsCategory.tsx
@@ -7,13 +7,17 @@ const ProductsCategory = (props: IProdCategory) => {
   return (
     <div className="flex flex-col w-full gap-[48px] mx-auto max-w-[90vw] md:max-w-[650px] pr-3 lg:max-w-[850px] xl:max-w-[1100px] md:ml-auto">
       <div className="flex-center justify-between">
-        <h2 className="font-[500] text-[30px]">{props.category.name}</h2>
+        <h2 className="font-[500] text-lg md:text-[30px]">
+          {props.category.name}
+        </h2>
 
         <Link
           to={`/dashboard/marketplace/${props.category.slug}`}
           className="w-fit flex-center gap-2 text-blue-400 underline"
         >
-          <button className="">View more</button>
+          <button className="text-sm md:text-base min-w-[100px]">
+            View more
+          </button>
           <GrLinkNext />
         </Link>
       </div>
