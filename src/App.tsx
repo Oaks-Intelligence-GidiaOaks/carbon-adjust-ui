7ba644bda80449951f7fc0d999cf28b9aa1e6b59
ImoDavid <emperorjagz@gmail.com>
2024-12-12 08:22:01 +0100

    feat: trademark added.

--- src/App.tsx
+++ src/App.tsx
@@ -32,13 +32,8 @@ function App() {
   }, [user?._id]);
 
   initParticlesEngine(async (engine:Engine) => {
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    //await loadAll(engine);
-    //await loadFull(engine);
+   
     await loadSlim(engine);
-    //await loadBasic(engine);
   }).then(() => {
     setInit(true);
   });
